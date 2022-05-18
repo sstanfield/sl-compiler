85f211b7fb9d1639e0943a635a18b2b65361149e
Steven Stanfield <stanfield@scarecrowtech.com>
2022-05-18 00:26:22 -0400

    Added vec-slice and vec->list.

--- slosh/src/main.rs
+++ slosh/src/main.rs
@@ -141,6 +141,59 @@ fn load(vm: &mut Vm, registers: &[Value]) -> VMResult<Value> {
     Ok(last)
 }
 
+fn vec_slice(vm: &mut Vm, registers: &[Value]) -> VMResult<Value> {
+    let (vector, start, end) = match registers.len() {
+        2 => {
+            if let (Value::Vector(vector), Ok(start)) = (registers[0], registers[1].get_int()) {
+                let v = vm.get_vector(vector);
+                (v, start as usize, v.len())
+            } else {
+                return Err(VMError::new_vm("vec-slice: Invalid arguments".to_string()));
+            }
+        }
+        3 => {
+            if let (Value::Vector(vector), Ok(start), Ok(end)) = (registers[0], registers[1].get_int(), registers[2].get_int()) {
+                let v = vm.get_vector(vector);
+                (v, start as usize, end as usize)
+            } else {
+                return Err(VMError::new_vm("vec-slice: Invalid arguments".to_string()));
+            }
+        }
+        _ => return Err(VMError::new_vm("vec-slice: Invalid arguments (requires two or three)".to_string())),
+    };
+    let len = vector.len();
+    if start == len && end <= len {
+        Ok(vm.alloc_vector(Vec::new()))
+    } else if start >= len || end > len {
+        Err(VMError::new_vm("vec-slice: Invalid arguments- out of bounds".to_string()))
+    } else {
+        let new_vec = vector[start..end].to_vec();
+        Ok(vm.alloc_vector(new_vec))
+    }
+}
+
+fn vec_to_list(vm: &mut Vm, registers: &[Value]) -> VMResult<Value> {
+    if registers.len() != 1 {
+        return Err(VMError::new_vm("vec->list: Invalid arguments (requires one vector)".to_string()));
+    }
+    if let Value::Vector(vhandle) = registers[0] {
+        let vector = vm.get_vector(vhandle).to_vec();
+
+        let mut last = Value::Nil;
+        for item in vector
+            .iter()
+            .rev()
+        {
+            let old_last = last;
+            last = vm.alloc_pair(*item, old_last);
+        }
+        Ok(last)
+    } else {
+        Err(VMError::new_vm("vec->list: Invalid arguments (requires one vector)".to_string()))
+    }
+
+}
+
 const PROMPT_FN: &str = "prompt";
 fn main() {
     let mut con = Context::new();
@@ -153,6 +206,8 @@ fn main() {
     vm.set_global("prn", Value::Builtin(CallFunc { func: prn }));
     vm.set_global("dasm", Value::Builtin(CallFunc { func: dasm }));
     vm.set_global("load", Value::Builtin(CallFunc { func: load }));
+    vm.set_global("vec-slice", Value::Builtin(CallFunc { func: vec_slice }));
+    vm.set_global("vec->list", Value::Builtin(CallFunc { func: vec_to_list }));
     //vm.set_global("eval", Value::Builtin(CallFunc { func: eval }));
     loop {
         let res = match con.read_line(Prompt::from("slosh> "), None) {
